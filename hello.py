2125a23c4ef20535bbaddb374756085605a8786e
Jose Sánchez <sanchez069@gmail.com>
2022-11-25 13:32:54 +0100

    Add helloPython and my first variables

---
+++ hello.py
@@ -0,0 +1,13 @@
+#Hola Python
+print("Hola Python")
+
+# Variables
+my_string_variable = "Las variables me las saco de los cojones amigo!."
+print("my_string_variable: " , my_string_variable)
+
+my_int_variable = 5
+print(my_int_variable)
+
+my_int_to_str_variable = str(my_int_variable)
+print("my_int_to_str_variable: " , my_int_to_str_variable)
+print("type(my_int_to_str_variable): " , type(my_int_to_str_variable))
