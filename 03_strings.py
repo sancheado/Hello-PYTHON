9ae83f706b69cb6891f225ab8edf2698c91709e4
Jose Sánchez Mateo <Sanchez069@gmail.com>
2022-12-05 10:32:58 +0100

    Finish Strings, Add DiscordBot

--- 03_strings.py
+++ 03_strings.py
@@ -23,3 +23,4 @@ print(my_scape_string)
 name, surname, age = "Jose", "Sánchez Mateo", 35
 print("Mi nombre es {} {} y mi edad es {}".format(name,surname,age))
 print("Mi nombre es {{(%s) [%s]}} y mi edad es _-[%s]-_" %(name,surname,age))
+print("Mi nombre es "+ name + surname +" y mi edad es "+ str(age))
